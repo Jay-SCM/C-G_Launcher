103ca2d8aded09dee872235fc22d6f510243e749
Jay-SCM <144235727+Jay-SCM@users.noreply.github.com>
2023-10-15 16:37:14 +0900

    Add files via upload
    Added Web functionality and error handling, when navigating the URL box (and a msgbox popup dialog that displays the error message when loading the page)--- NEED to add text to the url text box and clear the text after the search is made.

---
+++ Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿
+namespace Miruku_GL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBoxSlider = new System.Windows.Forms.PictureBox();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.tsmiGame1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiGame2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiGame3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSlider)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(12, 94);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(122, 627);
+            this.panel1.TabIndex = 0;
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.Location = new System.Drawing.Point(24, 242);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(64, 68);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.Location = new System.Drawing.Point(24, 118);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(64, 69);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.Location = new System.Drawing.Point(24, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(64, 60);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBoxSlider
+            // 
+            this.pictureBoxSlider.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBoxSlider.BackgroundImage")));
+            this.pictureBoxSlider.Location = new System.Drawing.Point(140, 94);
+            this.pictureBoxSlider.Name = "pictureBoxSlider";
+            this.pictureBoxSlider.Size = new System.Drawing.Size(1046, 627);
+            this.pictureBoxSlider.TabIndex = 1;
+            this.pictureBoxSlider.TabStop = false;
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Location = new System.Drawing.Point(140, 727);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(75, 23);
+            this.btnPrevious.TabIndex = 2;
+            this.btnPrevious.Text = "prev";
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(1111, 727);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 3;
+            this.btnNext.Text = "nxt";
+            this.btnNext.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Visible;
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(2648, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // tsmiGame1
+            // 
+            this.tsmiGame1.Name = "tsmiGame1";
+            this.tsmiGame1.Size = new System.Drawing.Size(48, 20);
+            this.tsmiGame1.Text = "WoW";
+            this.tsmiGame1.Click += new System.EventHandler(this.tsmiGame1_Click);
+            // 
+            // tsmiGame2
+            // 
+            this.tsmiGame2.Name = "tsmiGame2";
+            this.tsmiGame2.Size = new System.Drawing.Size(80, 20);
+            this.tsmiGame2.Text = "Satisfactory";
+            this.tsmiGame2.Click += new System.EventHandler(this.tsmiGame2_Click);
+            // 
+            // tsmiGame3
+            // 
+            this.tsmiGame3.Name = "tsmiGame3";
+            this.tsmiGame3.Size = new System.Drawing.Size(104, 20);
+            this.tsmiGame3.Text = "IhavNotDecided";
+            this.tsmiGame3.Click += new System.EventHandler(this.tsmiGame3_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(1192, 94);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(952, 627);
+            this.webBrowser1.TabIndex = 5;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(2069, 65);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 6;
+            this.btnGo.Text = "search";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(1203, 68);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(851, 20);
+            this.txtUrl.TabIndex = 7;
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(48, 20);
+            this.toolStripMenuItem1.Text = "WoW";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(80, 20);
+            this.toolStripMenuItem2.Text = "Satisfactory";
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.BackColor = System.Drawing.SystemColors.Highlight;
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(62, 20);
+            this.toolStripMenuItem3.Text = "Factorio";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(2648, 1125);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPrevious);
+            this.Controls.Add(this.pictureBoxSlider);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSlider)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBoxSlider;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem tsmiGame1;
+        private System.Windows.Forms.ToolStripMenuItem tsmiGame2;
+        private System.Windows.Forms.ToolStripMenuItem tsmiGame3;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;//using System.Collections.Generic;
+using System.Windows.Forms;//using System.Linq;
+using System.Drawing;//using System.Windows.
+namespace Miruku_GL//System.Windows.Forms
+{
+    public partial class Form1 : Form//System.Windows.Forms.Form
+    {
+        private string[] imageResourceNames = { "image1.jpg", "image2.jpg", "image3.jpg" };//private string[] imageResourceNames = { "image1.jpg", "image2.jpg", "image3.jpg" };
+        private int currentIndex = 0; // Index of the current image.
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Set the initial image using a resource name.
+            pictureBoxSlider.Image = Properties.Resources.image1;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Replace the path with the actual path to your Game 3 executable.
+            System.Diagnostics.Process.Start(@"C:\Program Files (x86)\World of Warcraft\World of Warcraft Launcher.exe");
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Replace the path with the actual path to your Game 2 executable.
+            System.Diagnostics.Process.Start(@"C:\Program Files (x86)\World of Warcraft\World of Warcraft Launcher.exe");
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Replace the path with the actual path to your Game 1 executable.
+            System.Diagnostics.Process.Start(@"C:\Program Files (x86)\World of Warcraft\World of Warcraft Launcher.exe");
+        }
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            // Show the next image.
+            currentIndex = (currentIndex + 1) % imageResourceNames.Length; // Increment the index and wrap around to 0 if necessary.
+            pictureBoxSlider.Image = (Image)Properties.Resources.ResourceManager.GetObject(imageResourceNames[currentIndex]); // Get the image from the resources and show it.
+        }
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            // Show the previous image.
+            currentIndex = (currentIndex - 1 + imageResourceNames.Length) % imageResourceNames.Length;
+            pictureBoxSlider.Image = (Image)Properties.Resources.ResourceManager.GetObject(imageResourceNames[currentIndex]);
+        }
+        private void tsmiGame1_Click(object sender, EventArgs e)
+        {
+            // Code to handle the click on Game 1.
+
+            // You can launch your game here.
+            System.Diagnostics.Process.Start(@"C:\Program Files (x86)\World of Warcraft\World of Warcraft Launcher.exe");
+        }
+        private void tsmiGame2_Click(object sender, EventArgs e)
+        {
+            // Code to handle the click on Game 2.
+            // You can launch your second game here.
+            System.Diagnostics.Process.Start(@"C:\Program Files (x86)\World of Warcraft\World of Warcraft Launcher.exe");
+        }
+        private void tsmiGame3_Click(object sender, EventArgs e)
+        {
+            // Code to handle the click on Game 3.
+
+            // You can launch your third game here.
+            System.Diagnostics.Process.Start(@"C:\Program Files (x86)\World of Warcraft\World of Warcraft Launcher.exe");
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            string url = txtUrl.Text;
+            try
+            {
+                webBrowser1.Navigate(new Uri(url));
+            }
+            catch (UriFormatException)
+            {
+                // Handle invalid URL format.
+                MessageBox.Show("Invalid URL format");
+            }
+            catch (Exception ex)
+            {
+                // Handle other exceptions, e.g., network issues.
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+        private void StartGame(string path)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(path);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+    }
+}
